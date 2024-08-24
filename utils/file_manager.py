5b5e80e794cd7e23ede10e705248cdc59900f268
kwasinsiah <truegrayowl@gmail.com>
2024-08-24 03:26:15 -0400

    Added basic file read/right/append functions

--- utils/file_manager.py
+++ utils/file_manager.py
@@ -4,30 +4,40 @@ import os.path
 
 class FileManager:
 
+    # Read a file
+    def read_file(self, file_name):
+        with open(file_name, 'r') as f:
+            return f.read()
+
+    #   Write to a file
+    def write_file(self, file_name):
+        with open(file_name, 'w') as f:
+            return f.write()
+
+
+    #   Update/ append to a file
+    def append_file(self, file_name):
+        with open(file_name, 'a') as f:
+            return f.write()
+
+    # Read a file then append to it
+    def read_write_file(self, file_name):
+        with open(file_name, 'r+') as f:
+            return f.read()
+
+
+
     def clean_json(self, json_file):
         content = json_file.strip("```json").strip("```").strip()
-        # with open('res.txt', 'r+') as file:
-        #     # Read the current content
-        #     content = file.read()
-        #
-        #     print("----------------------")
-        #     print("Current content:", content)
-        #     content = content.strip("```json").strip("```").strip()
-        #     print("Current content:", content)
-        #     print("----------------------")
-        #     file.write(content)
-        #
-        #     file.close()
         return content
-        # print("----------------------")
-        # print(json_file)
-        # print("----------------------")
+
     def dict_to_json(self,data):
         json_string = json.dumps(data, indent=4)
         return json_string
 
     def string_to_json(self,string):
         string = self.clean_json(string)
+        print("String to convert to json", string)
         return json.loads(string)
 
     def update_page_content(self,content):
